5c6ee342a987a9aa47d29c5ec7aa5488dafbf713
Beenyaa <bencegadanyi1@hotmail.com>
2022-09-11 22:38:05 +0100

    fix: tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
-  content: ['./src/routes/**/*.{svelte,js,ts}'],
+  content: ['./src/**/*.{tsx,ts}'],
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['acid'],
+  },
 };
